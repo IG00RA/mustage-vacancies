233928ae246f46178b6a259ace57125f82272cbb
Ihor Hromadskiy <gromadskiyigor@gmail.com>
2025-01-03 18:22:48 +0200

    [Add] offer section

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -8,20 +8,40 @@ export default function Offer() {
 
   return (
     <section id="offer" className={styles.offer}>
-      <div className={styles.container}>
-        <span className={styles.headerText}>{t('Offer.headerText')}</span>
-        <h2 className={styles.header}>{t('Offer.header')}</h2>
-        <ul className={styles.list}>
-          {offerItems.map((item, index) => (
-            <li key={index}>
-              <div className={styles.icon_wrap}>
-                <Icon name={item.logo} width={32} height={32} />
-              </div>
-              <h3 className={styles.list_header}>{t(item.header)}</h3>
-            </li>
-          ))}
-        </ul>
+      <h2 className={styles.header}>{t('Offer.header')}</h2>
+      <div className={styles.back}></div>
+      <div className={styles.container_wrap}>
+        <div className={styles.first_container}>
+          <p className={styles.container_text}>
+            {t('Offer.blockText.first.first')}
+            <span>{t('Offer.blockText.first.second')}</span>
+            {t('Offer.blockText.first.third')}
+          </p>
+        </div>
+        <div className={styles.second_container}>
+          <p className={styles.container_text}>
+            <span>{t('Offer.blockText.second.first')}</span>
+            {t('Offer.blockText.second.second')}
+          </p>
+        </div>
       </div>
+      <div className={styles.third_container}>
+        <p className={styles.container_text}>
+          <span>{t('Offer.blockText.third.first')}</span>
+          {t('Offer.blockText.third.second')}
+        </p>
+      </div>
+
+      <h3 className={styles.list_text}>{t('Offer.headerText')}</h3>
+      <ul className={styles.list}>
+        {offerItems.map((item, index) => (
+          <li className={styles.list_item} key={index}>
+            <Icon name={item.logo} width={24} height={24} />
+
+            <h3 className={styles.list_header}>{t(item.header)}</h3>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
